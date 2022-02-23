eb64f95646ebfb03cccc0123f1aeea03e1c5aab3
pep <peter.teleki.farkas@pannongreenpower.hu>
2022-02-23 14:07:46 +0100

    querying for the last publish date

--- huf2eur.py
+++ huf2eur.py
@@ -1,11 +1,13 @@
+from datetime import date
 from zeep import Client
 import xml.etree.ElementTree as ET
 
 def __main__():
     client = Client('http://www.mnb.hu/arfolyamok.asmx?wsdl')
+    last_publish_date = get_last_publish_date(client)
     xr = client.service.GetExchangeRates(
-        startDate = '2022-01-10', 
-        endDate = '2022-01-10', 
+        startDate = last_publish_date, 
+        endDate = last_publish_date, 
         currencyNames='EUR'
     )
     etree = ET.fromstring(xr)
@@ -13,6 +15,13 @@ def __main__():
     rate_str = etree.find('Day').find('Rate').text
     rate = float(rate_str.replace(',', '.'))
     print(rate)
+
+def get_last_publish_date(client : Client):
+    date_interval_xml_str = client.service.GetDateInterval()
+    # <MNBStoredInterval><DateInterval startdate="1949-01-03" enddate="2022-02-23" /></MNBStoredInterval>
+    date_interval_etree = ET.fromstring(date_interval_xml_str)
+    end_date = date_interval_etree.find('DateInterval').attrib['enddate']
+    return end_date
 
 if __name__ == '__main__':
     __main__()
